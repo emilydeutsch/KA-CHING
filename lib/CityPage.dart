de06f4005156055e5ea619121a73fbf540eaaffa
Michelle Li <45521335+floatingturnip@users.noreply.github.com>
2020-10-17 17:15:47 -0700

    Dropdown menu for Job and City

--- lib/CityPage.dart
+++ lib/CityPage.dart
@@ -10,13 +10,48 @@ class CityPage extends StatefulWidget {
 }
 
 class _CityPageState extends State<CityPage> {
+  String dropdownValue = 'Select a City';
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Center(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
+          //Text("Your Job selected:"),
+          //Text(widget.userSelection[0]),
           Text("City"),
+          Container(
+            //width: 300.0,
+            //color: Colors.lightBlue,
+            child: DropdownButton(
+              value: dropdownValue,
+              icon: Icon(Icons.arrow_downward),
+              iconSize: 20,
+              elevation: 0,
+              /*style: TextStyle(
+                color: Colors.deepPurple,
+              ),*/
+              //dropdownColor: Colors.lightBlue[50],
+              underline: Container(
+                height: 2,
+                color: Colors.deepPurpleAccent,
+              ),
+              onChanged: (String newValue) {
+                setState(() {
+                  dropdownValue = newValue;
+                });
+                widget.userSelection[1] = dropdownValue;
+
+              },
+              items: <String>['Select a City', 'Seattle', 'New York', 'Kansas City']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          ),
           RaisedButton(
               child: Text("next"),
               onPressed: () {
--- lib/JobPage.dart
+++ lib/JobPage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:katching_app/CityPage.dart';
 import 'package:katching_app/SlideTransition.dart';
-
+// TODO: Make this less ugly
 // ignore: must_be_immutable
 class JobPage extends StatefulWidget {
   List<String> userSelection;
@@ -11,7 +11,7 @@ class JobPage extends StatefulWidget {
 }
 
 class _JobPageState extends State<JobPage> {
-
+  String dropdownValue = 'Select a Job';
   @override
   Widget build(BuildContext context) {
     return Scaffold(body: Center(
@@ -19,6 +19,38 @@ class _JobPageState extends State<JobPage> {
           mainAxisSize: MainAxisSize.min,
             children: <Widget>[
           Text("Job"),
+          Container(
+            //width: 300.0,
+            //color: Colors.lightBlue,
+            child: DropdownButton(
+              value: dropdownValue,
+              icon: Icon(Icons.arrow_downward),
+              iconSize: 20,
+              elevation: 0,
+              /*style: TextStyle(
+                color: Colors.deepPurple,
+              ),*/
+              //dropdownColor: Colors.lightBlue[50],
+              underline: Container(
+                height: 2,
+                color: Colors.deepPurpleAccent,
+              ),
+              onChanged: (String newValue) {
+                setState(() {
+                  dropdownValue = newValue;
+                });
+                widget.userSelection[0] = dropdownValue;
+
+              },
+              items: <String>['Select a Job', 'Teacher', 'Doctor', 'Artist']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          ),
           RaisedButton(
             child:Text("next"),
             onPressed: () {
@@ -33,3 +65,75 @@ class _JobPageState extends State<JobPage> {
   }
 }
 
+/// This is the stateful widget that the main application instantiates.
+class SelectOccupation extends StatefulWidget {
+  SelectOccupation({Key key}) : super(key: key);
+
+  @override
+  _SelectOccupationState createState() => _SelectOccupationState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _SelectOccupationState extends State<SelectOccupation> {
+  String dropdownValue = 'Select a Job';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300.0,
+      color: Colors.lightBlue,
+      child: DropdownButton(
+        value: dropdownValue,
+        icon: Icon(Icons.arrow_downward),
+        iconSize: 20,
+        elevation: 0,
+        style: TextStyle(
+            color: Colors.deepPurple,
+        ),
+        dropdownColor: Colors.lightBlue[50],
+        underline: Container(
+          height: 2,
+          color: Colors.deepPurpleAccent,
+        ),
+        onChanged: (String newValue) {
+          setState(() {
+            dropdownValue = newValue;
+          });
+
+        },
+        items: <String>['Select a Job', 'Teacher', 'Doctor', 'Artist']
+            .map<DropdownMenuItem<String>>((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        }).toList(),
+      ),
+    );
+    /*
+    DropdownButton<String>(
+      value: dropdownValue,
+      icon: Icon(Icons.arrow_downward),
+      iconSize: 20,
+      elevation: 0,
+      style: TextStyle(color: Colors.deepPurple),
+      dropdownColor: Colors.lightBlue[50],
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String newValue) {
+        setState(() {
+          dropdownValue = newValue;
+        });
+      },
+      items: <String>['Select a Job', 'Teacher', 'Doctor', 'Artist']
+          .map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    ); */
+  }
+}
