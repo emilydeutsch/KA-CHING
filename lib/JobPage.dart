ff064d995f3f82bb223dc266b5b6f7ea7288e83e
Michelle Li <45521335+floatingturnip@users.noreply.github.com>
2020-10-17 18:18:29 -0700

    Force user to make a dropdown selection

--- lib/JobPage.dart
+++ lib/JobPage.dart
@@ -54,86 +54,16 @@ class _JobPageState extends State<JobPage> {
           RaisedButton(
             child:Text("next"),
             onPressed: () {
-              Navigator.push(context,
-                  SlideRightRoute(page: CityPage(userSelection:widget.userSelection)));
+              if(dropdownValue != "Select a Job") {
+                Navigator.push(context,
+                    SlideRightRoute(
+                        page: CityPage(userSelection: widget.userSelection)));
+              }
             }
           ),
         ],
       ),
     ),
     );
-  }
-}
-
-/// This is the stateful widget that the main application instantiates.
-class SelectOccupation extends StatefulWidget {
-  SelectOccupation({Key key}) : super(key: key);
-
-  @override
-  _SelectOccupationState createState() => _SelectOccupationState();
-}
-
-/// This is the private State class that goes with MyStatefulWidget.
-class _SelectOccupationState extends State<SelectOccupation> {
-  String dropdownValue = 'Select a Job';
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 300.0,
-      color: Colors.lightBlue,
-      child: DropdownButton(
-        value: dropdownValue,
-        icon: Icon(Icons.arrow_downward),
-        iconSize: 20,
-        elevation: 0,
-        style: TextStyle(
-            color: Colors.deepPurple,
-        ),
-        dropdownColor: Colors.lightBlue[50],
-        underline: Container(
-          height: 2,
-          color: Colors.deepPurpleAccent,
-        ),
-        onChanged: (String newValue) {
-          setState(() {
-            dropdownValue = newValue;
-          });
-
-        },
-        items: <String>['Select a Job', 'Teacher', 'Doctor', 'Artist']
-            .map<DropdownMenuItem<String>>((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Text(value),
-          );
-        }).toList(),
-      ),
-    );
-    /*
-    DropdownButton<String>(
-      value: dropdownValue,
-      icon: Icon(Icons.arrow_downward),
-      iconSize: 20,
-      elevation: 0,
-      style: TextStyle(color: Colors.deepPurple),
-      dropdownColor: Colors.lightBlue[50],
-      underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
-      ),
-      onChanged: (String newValue) {
-        setState(() {
-          dropdownValue = newValue;
-        });
-      },
-      items: <String>['Select a Job', 'Teacher', 'Doctor', 'Artist']
-          .map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
-    ); */
   }
 }
